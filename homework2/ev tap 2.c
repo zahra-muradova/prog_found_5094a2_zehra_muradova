7052b462c1002f7952ab3e95b1658848ed2b316c
zahra-muradova <zmuradova@std.beu.edu.az>
2024-10-18 19:33:24 +0400

    Add files via upload

---
+++ homework2/ev tap 2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+    char name[]="zehra";
+    int i=16;
+    char word[]= "O";
+    printf("Menim adim %s'dir\n", name);
+    printf("Menim %d yasim var\n", i);
+    printf("Bu ay %s herfile baslayir", word);
+    return 0;
+}
