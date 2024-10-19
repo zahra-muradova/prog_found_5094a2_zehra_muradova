f22ef6ca1d5445a88dd330e4b62377eb701f307f
zahra-muradova <zmuradova@std.beu.edu.az>
2024-10-19 13:03:38 +0400

    Add files via upload

---
+++ homework3/ev tap 3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+    int a=5;
+    int b=8;
+    int c=15;
+    int sum=a+b+c;
+    printf("sum=%d\n", sum);
+    const float PI=3.14;
+    printf("const=%f\n", PI);
+    printf("a+1=%d\n", a+1);
+}
